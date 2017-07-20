929c888d0dc0d6433f5233f58555f1cd98b2dbc9
Ashwin Boini <ashwinboini@yahoo.com>
2017-07-20 16:03:12 -0400

    Cmmt Msg

---
+++ src/test/java/com/fanniemae/testcases/DummyTestCase.java
@@ -0,0 +1,14 @@
+package com.fanniemae.testcases;
+
+import com.fanniemae.base.TestBase;
+import org.testng.annotations.Test;
+
+@Test
+public class DummyTestCase extends TestBase {
+
+	@Test(enabled=false)
+	public void tc_LoginTestCase() {
+
+	}
+
+}
---
+++ src/test/java/com/fanniemae/testcases/SDET_Login_TestCase.java
@@ -0,0 +1,28 @@
+/*package com.fanniemae.testcases;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SDET_Login_TestCase extends TestBase	{
+	
+	String DBDataInLocalArray[][]=null;
+	int numOfRows = 0;
+
+	
+	@Test(enabled=false)
+	public void testBeginnerSDETLogin() throws Exception {
+		System.setProperty("webDriver.firefox.marionette", "C:\\ALLJARS\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("")
+	}
+	
+	@Test(enabled=true)
+	public void test_DDT_SDETLogin() throws Exception {
+		
+		FetchDBData();
+	}
+		System.setProperty("webDriver.firefox.marionette", "C:\\ALLJARS\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("")
+}
+*/
